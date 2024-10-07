e2a60df73d958e48163bf4ee722949756ae0b7bf
Elisa Carvalho <elisa03carvalho@gmail.com>
2024-10-07 10:16:04 -0300

    feat: Crud

---
+++ src/main/java/sp/gov/sp/fatec/springatv20242/Springatv20242Application.java
@@ -0,0 +1,13 @@
+package sp.gov.sp.fatec.springatv20242;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springatv20242Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springatv20242Application.class, args);
+	}
+
+}
---
+++ src/test/java/sp/gov/sp/fatec/springatv20242/Springatv20242ApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.gov.sp.fatec.springatv20242;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springatv20242ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
